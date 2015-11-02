15fae18d773277a665e86a5a354f0ea3ac7a701c
Corinne Dunston <corinnedunston@corinnes-imac.local>
2015-11-02 00:00:29 -0500

    Initial Commit

---
+++ Sparc Tip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sparc Tip
+//
+//  Created by Corinne Dunston on 11/2/15.
+//  Copyright © 2015 CK Sparc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
